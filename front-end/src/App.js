197e79f3177b238fac85f159ebb323c3484cdb6c
Mikołaj Jonczyk <mikolaj.jonczyk@apsis.com>
2023-07-09 15:49:18 +0200

    Draft for get request to be

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import { Form } from './components/Form';
 import { Slider } from './components/Slider'
 import { SliderLabel } from './components/container-styles/styles';
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">  
-        <Form>
+        <Form onSubmit={handleSubmit}>
         </Form>
         <SliderLabel>Select your preferences:</SliderLabel>
         <Slider left="Active" right="Relaxed"></Slider>
@@ -16,5 +17,27 @@ function App() {
     </div>
   );
 }
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  // Get the form data
+  const formData = new FormData(event.target);
+  const city = formData.get('city');
+  const email = formData.get('email');
+
+  // Send the POST request
+  try {
+    const response = await axios.get('http://localhost:8000', {
+      params: {
+        city,
+        email,
+      },
+    });
+    console.log(response.data); // Handle the response
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export default App;
--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -5,7 +5,7 @@ function Form(props) {
     <FormContainer>
       <Title>Travel AI</Title>
       <SearchForm>
-        <SearchInput type="text" placeholder="Search for your destination..." />
+        <SearchInput type="text" name="city" placeholder="Search for your destination..." />
         <SearchButton type="submit">Search</SearchButton>
         {props.children}
       </SearchForm>
