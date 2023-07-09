25f7904e3acaa6020d7df7231bcd747acef8784c
Mikołaj Jonczyk <mikolaj.jonczyk@apsis.com>
2023-07-09 22:40:21 +0200

    style changes

--- front-end/src/components/container-styles/attractions-styles.js
+++ front-end/src/components/container-styles/attractions-styles.js
@@ -1,13 +1,57 @@
 import styled from "styled-components"
 
-const AttractionsContainer = styled.div`
+const AttractionsItem = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px; 
+  background-color: #f2f2f2;
+  border-style: solid;
+  border-width: 2px;
+  border-radius: 15px;
+  border-color: #3C372C;
+  margin: 5px;
+  `
+
+ const AttractionsContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #f2f2f2;
+  background-color: #EFEDE3;
+  max-width: 530px;
   `
 
+  const AttractionsName = styled.h1`
+  font-size: 1.3rem;
+  margin: 1.5rem;
+  text-decoration: underline;
+  `
+
+  const AttractionsDescription = styled.div`
+  grid-area: 2 / 1 / 3 / 4;
+  font-size: 1rem;
+  margin: 0.2rem;
+  `
+
+  const AttractionsNameLocation = styled.div`
+  grid-area: 1 / 2 / 2 / 4;
+  font-size: 1.6rem;
+  margin: 0.2rem;
+`
+  
+const AttractionsImage = styled.div`
+  grid-area: 1 / 1 / 2 / 2;
+  margin: 0.2rem;
+  `
+
+
   export {
+    AttractionsItem,
     AttractionsContainer,
+    AttractionsName,
+    AttractionsDescription,
+    AttractionsImage,
+    AttractionsNameLocation
   } 
